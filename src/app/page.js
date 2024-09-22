91345e833b145f461842fe5e3b9b1d07d9e92dbf
Rizr09 <rizky2004cool@gmail.com>
2024-09-23 05:44:40 +0700

    ench: added docs

--- src/app/page.js
+++ src/app/page.js
@@ -67,8 +67,16 @@ export default function Home() {
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <h1 className="text-2xl font-semibold text-black mb-5">
-              <a href='https://www.researchgate.net/publication/321757985_InSet_Lexicon_Evaluation_of_a_Word_List_for_Indonesian_Sentiment_Analysis_in_Microblogs' target='_blank' rel="noopener noreferrer" className="">
-                Analisis Sentimen Menggunakan <span className='hover:text-indigo-600 transition-colors underline decoration-violet-600'>InSet (Indonesia Sentiment Lexicon)</span>
+              <a
+                href="https://www.researchgate.net/publication/321757985_InSet_Lexicon_Evaluation_of_a_Word_List_for_Indonesian_Sentiment_Analysis_in_Microblogs"
+                target="_blank"
+                rel="noopener noreferrer"
+                className=""
+              >
+                Analisis Sentimen Menggunakan{' '}
+                <span className="hover:text-indigo-600 transition-colors underline decoration-violet-600">
+                  InSet (Indonesia Sentiment Lexicon)
+                </span>
               </a>
             </h1>
 
@@ -76,7 +84,31 @@ export default function Home() {
 
             <div className="space-y-6">
               <div className="flex flex-col">
-                <label htmlFor="text-input" className="text-sm font-medium text-gray-700 mb-1">Teks untuk dianalisis, untuk referensi preprocessing liat <a href='https://github.com/agushendra7/twitter-sentiment-analysis-using-inset-and-random-forest' target='_blank' className='underline'>disini</a></label>
+                <label
+                  htmlFor="text-input"
+                  className="text-sm font-medium text-gray-700 mb-1"
+                >
+                  Masukkan teks untuk dianalisis, untuk referensi preprocessing lihat{' '}
+                  <a
+                    href="https://github.com/agushendra7/twitter-sentiment-analysis-using-inset-and-random-forest"
+                    target="_blank"
+                    className="underline"
+                  >
+                    disini
+                  </a>
+                  , atau hit endpoint {' '}
+                  <code className="bg-gray-200 p-1 rounded-md text-indigo-600 break-all">
+                      https://inset-sentiment-analysis.vercel.app/api/analyze
+                  </code>{' '}
+                  dengan method {' '}
+                  <code className="bg-gray-200 p-1 rounded-md text-indigo-600 break-all">
+                    POST
+                  </code>
+                  {' '}dan body sebagai berikut:
+                </label>
+                <SyntaxHighlighter language="json" style={duotoneDark} className="bg-gray-100 p-2 rounded-md">
+                  {`{\n  "text": "halo dunia"\n}`}
+                </SyntaxHighlighter>
                 <textarea
                   id="text-input"
                   className="px-4 py-2 border focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md text-black"
@@ -93,13 +125,15 @@ export default function Home() {
               >
                 {loading ? 'Menganalisis...' : 'Analisis Sentimen'}
               </button>
-              {error && (
-                <div className="text-red-600 text-sm">{error}</div>
-              )}
+              {error && <div className="text-red-600 text-sm">{error}</div>}
               {result && (
                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                   <h2 className="text-xl font-semibold">Hasil Analisis:</h2>
-                  <SyntaxHighlighter language="json" style={duotoneDark} className="bg-gray-100 p-4 rounded-md overflow-auto">
+                  <SyntaxHighlighter
+                    language="json"
+                    style={duotoneDark}
+                    className="bg-gray-100 p-4 rounded-md overflow-auto"
+                  >
                     {JSON.stringify(result, null, 2)}
                   </SyntaxHighlighter>
                 </div>
